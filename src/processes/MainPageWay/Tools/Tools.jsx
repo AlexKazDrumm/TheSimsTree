0f6a1de57cb2690383e9314843ce6789135aae36
Agarey <alexdrumm13@gmail.com>
2023-10-25 11:55:56 +0600

    tools icons

--- src/processes/MainPageWay/Tools/Tools.jsx
+++ src/processes/MainPageWay/Tools/Tools.jsx
@@ -9,18 +9,27 @@ const Tools = () => {
         "Создание древа": ["Шаг A", "Шаг B"],
         "Выгрузка и загрузка": ["ауцаа", "уцкпцука"],
         "Общение и коммьюнити": ["фауц", "йцукп"],
-        "Ещё один блок": ["1324A", "Ша1234"],
+        "FAQ": ["1324A", "Ша1234"],
     };
+
+    const themeIcons = {
+        "Регистрация и авторизация": '/svg/tools_settings.svg',
+        "Создание древа": '/svg/tools_plum_tree.svg',
+        "Выгрузка и загрузка": '/svg/tools_download.svg',
+        "Общение и коммьюнити": '/svg/tools_chat.svg',
+        "FAQ": '/svg/tools_faq.svg',
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.titleRow}>
                 <span className={styles.title}>Инструкции</span>
             </div>
             <div className={styles.themesRow}>
-                {["Регистрация и авторизация", "Создание древа", "Выгрузка и загрузка", "Общение и коммьюнити", "Ещё один блок"].map(theme => (
+                {["Регистрация и авторизация", "Создание древа", "Выгрузка и загрузка", "Общение и коммьюнити", "FAQ"].map(theme => (
                     <div className={styles.theme} key={theme} style={theme == selectedTheme ? {border: '3px solid green'} : {border: 'none'}} onClick={() => setSelectedTheme(theme)}>  
                         <div className={styles.imgBlock}>
-                            <img className={styles.logo} src='/svg/gem.svg' />
+                            <img className={styles.logo} src={themeIcons[theme]} />
                         </div>
                         <div className={styles.spanBlock}>
                             <span>{theme}</span>
