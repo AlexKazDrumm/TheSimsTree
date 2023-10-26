88f628e5568656231dcdc04c28199bfdfc1b9486
Agarey <alexdrumm13@gmail.com>
2023-10-27 02:17:44 +0600

    contacts

--- src/processes/MainPageWay/Contacts/Contacts.jsx
+++ src/processes/MainPageWay/Contacts/Contacts.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import styles from './Contacts.module.css'
-import PageNotFound from "../../../components/PageNotFound/PageNotFound";
+import styles from './Contacts.module.css';
 
 const Contacts = () => {
-
     return (
-        <>
-            <PageNotFound />
-        </>
-        // <div className={styles.container}>
-        //     <div className={styles.titleRow}>
-        //         <span className={styles.title}>С нами всегда просто связаться!</span>
-        //     </div>
-        // </div>
-    )
-}
+        <div className={styles.container}>
+            <div className={styles.titleRow}>
+                <span className={styles.title}>С нами всегда просто связаться!</span>
+            </div>
+            <div className={styles.contactsRow}>
+                <div className={`${styles.contact} ${styles.yout}`} style={{ borderColor: '#FF4B4B' }}></div>
 
-export default Contacts
+                <a href="https://t.me/dynastytree" target="_blank" rel="noopener noreferrer" className={`${styles.contact} ${styles.telegram}`} style={{ borderColor: '#37BBFE' }}></a>
+
+                <div className={`${styles.contact} ${styles.vk}`} style={{ borderColor: '#2789F6' }}></div>
+            </div>
+        </div>
+    );
+};
+
+export default Contacts;
