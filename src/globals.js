c117de985673ab0695a43e37849a0991c1d33cd1
Agarey <alexdrumm13@gmail.com>
2023-11-15 22:51:39 +0600

    domain

--- src/globals.js
+++ src/globals.js
@@ -1,10 +1,10 @@
 export default {
     // productionServerDomain: "http://localhost:3030",
-    productionServerDomain: "https://95.163.229.83:3030",
+    productionServerDomain: "https://simsdynastytree.online",
 
-    // productionSiteDomain: "https://the-sims-tree.vercel.app/",
-    productionSiteDomain: "http://localhost:3000",
+    productionSiteDomain: "https://the-sims-tree.vercel.app/",
+    // productionSiteDomain: "http://localhost:3000",
 
     // ftpDomain: "http://localhost:3030",
-    ftpDomain: "http://95.163.229.83:3030"
+    ftpDomain: "https://simsdynastytree.online"
 }
